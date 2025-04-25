12cfaf57edd28a96a25ef05f8fd8f8c595a760a4
masterOrigen <167365847+masterOrigen@users.noreply.github.com>
2025-04-25 11:11:59 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3000,
-    host: '0.0.0.0'
+    port: 3000,
+    host: true
   },
   preview: {
-    port: process.env.PORT || 3000,
-    host: '0.0.0.0'
+    port: 3000,
+    host: true
   }
 })
